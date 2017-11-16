7a995976bb28cb1ec2be328537b2aa1e13089b2a
stuartZhang <stuartpage@qq.com>
2017-11-16 23:22:57 +0800

    Complete a new function "loadTpslib"

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const java = require('java');
+const path = require('path');
+const fs = require('fs');
+const {promisify} = require('util');
 const _ = require('underscore');
+const fsStat = promisify(fs.stat);
 java.asyncOptions = {
   'asyncSuffix': 'Async',     // Don't generate node-style methods taking callbacks
   'syncSuffix': 'Sync',              // Sync methods use the base name(!!)
@@ -24,17 +28,19 @@ java.options.push('-Dfile.encoding=UTF-8');
  * </ul>
  */
 const Long = java.import('java.lang.Long');
+const StaticTemplateLibrary = java.import('com.nim.tps.StaticTemplateLibrary');
 const MutableTPSElement = java.import('com.nim.tps.MutableTPSElement');
 const tpsstr = java.import('com.nim.tps.attrio.tpsstr');
 const tpslong = java.import('com.nim.tps.attrio.tpslong');
 
-async function buildIdendTps({ip, lang, mdn, credential, 'client-guid': clientGuid}){
+async function buildIdendTps({ip, lang, mdn, credential, 'client-guid': clientGuid, 'user-agent': userAgent}){
   const idenTps = new MutableTPSElement('iden');
   await Promise.all([
     tpsstr.setPromise(idenTps , 'ip-address', ip),
     tpsstr.setPromise(idenTps , 'language', lang),
     tpsstr.setPromise(idenTps , 'credential', credential),
     tpsstr.setPromise(idenTps , 'client-guid', clientGuid),
+    tpsstr.setPromise(idenTps , 'user-agent', userAgent),
     new Promise((resolve, reject) => {
       if (_.isString(mdn) && !_.isEmpty(mdn)) {
         tpslong.setPromise(idenTps , 'mdn', new Long(mdn)).then(resolve, reject);
@@ -45,17 +51,62 @@ async function buildIdendTps({ip, lang, mdn, credential, 'client-guid': clientGu
   ]);
   return idenTps;
 }
+async function loadTpslib({tpslib}){
+  tpslib = path.resolve('./conf', tpslib);
+  const stats = await fsStat(tpslib);
+  if (!stats.isFile()) {
+    throw new Error(`tpslib ${tpslib} isn't a file.`);
+  }
+  return StaticTemplateLibrary.loadResourcePromise(tpslib);
+}
+/**
+ * Assume
+ * 1. apikey: 24611
+ */
 (async () => {
-  /**
-   * Assume
-   * 1. apikey: 24611
-   */
-  const idenTps = await buildIdendTps({
-    ip: '192.168.84.233',
-    lang: 'zh-cn',
-    mdn: '9999990012', // As for apikey=24611, the field should be null.
-    credential: 'tAO+lTVRKdtcVmX9hj1NTqfvz/FCx/vMtx4BfElp',
-    'client-guid': 'dc5b30a4ff374279802a06b26cef9b77'
-  }); //TODO: from http request
-  idenTps.toStringPromise().then(console.dir);
+  const request = {
+    iden: {
+      ip: '192.168.84.233',
+      lang: 'zh-cn',
+      mdn: '9999990012', // As for apikey=24611, the field should be null.
+      credential: 'tAO+lTVRKdtcVmX9hj1NTqfvz/FCx/vMtx4BfElp',
+      'client-guid': 'dc5b30a4ff374279802a06b26cef9b77',
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36'
+    },
+    servlet: {
+      /*
+        servlet name:
+          asr=asr-reco
+          event=proxpoi,global
+          geocode=geocode,global
+          map=map,global
+          movie=proxpoi,global
+          navigation=nav,global
+          proxpoi=proxpoi,global
+          com.nim.nbwa.servlet.vectortile=vector-tile,global
+          proxpoi.scheme=atlasbook-standard-2
+          pronun=pronun
+          singlesearch.scheme=tcs-nbwa-single-search
+          singlesearch.geocode.scheme=tcs-nbwa-geocode-search
+          traffic=proxpoi,sb
+          traffic.notify=traffic-notify,global
+          traffic.scheme=traffic-incident
+          reverseGeocode=reverse-geocode,global
+          sendPlace=send-place-message
+          sendMessage=send-message
+          weather=proxpoi,global
+          metadata=metadata
+          siinglesearch=search
+      */
+      name: 'geocode,global',
+      tpslib: '5a5139403cce4a4719428b304739567dab9e11de',
+      host: '192.168.84.234'
+    }
+  }; //TODO: from http request
+  const [idenTps, tpslib] = await Promise.all([
+    buildIdendTps(request.iden),
+    loadTpslib(request.servlet)
+  ]);
+  idenTps.toStringPromise().then(console.log);
+  console.dir(tpslib);
 })();

aa0a9e8079318651a63e170d6d6186a90e609bd1
Stuart (Haoyu)Zhang <stzhang@tjnimone.com>
2017-11-16 10:08:02 +0800

    Complete the function "buildIdenTps"

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const java = require('java');
+const _ = require('underscore');
 java.asyncOptions = {
   'asyncSuffix': 'Async',     // Don't generate node-style methods taking callbacks
-  'syncSuffix': "Sync",              // Sync methods use the base name(!!)
-  'promiseSuffix': "Promise",   // Generate methods returning promises, using the suffix Promise.
-  'promisify': require("when/node").lift
+  'syncSuffix': 'Sync',              // Sync methods use the base name(!!)
+  'promiseSuffix': 'Promise',   // Generate methods returning promises, using the suffix Promise.
+  'promisify': require('when/node').lift
 };
 java.classpath.push('./jars/nimtps-all.jar');
 java.options.push('-Dfile.encoding=UTF-8');
@@ -22,17 +23,39 @@ java.options.push('-Dfile.encoding=UTF-8');
  *   <li>com.nim.tps.attrio.tpsuint
  * </ul>
  */
+const Long = java.import('java.lang.Long');
 const MutableTPSElement = java.import('com.nim.tps.MutableTPSElement');
 const tpsstr = java.import('com.nim.tps.attrio.tpsstr');
-//
-async function buildIdendTps(){
-  const idenTps = new MutableTPSElement("iden");
-  await tpsstr.setPromise(idenTps , "ip-address", '192.168.84.233'); //TODO: from http request
-  await tpsstr.setPromise(idenTps , "language", 'zh-cn'); //TODO: from http request
+const tpslong = java.import('com.nim.tps.attrio.tpslong');
+
+async function buildIdendTps({ip, lang, mdn, credential, 'client-guid': clientGuid}){
+  const idenTps = new MutableTPSElement('iden');
+  await Promise.all([
+    tpsstr.setPromise(idenTps , 'ip-address', ip),
+    tpsstr.setPromise(idenTps , 'language', lang),
+    tpsstr.setPromise(idenTps , 'credential', credential),
+    tpsstr.setPromise(idenTps , 'client-guid', clientGuid),
+    new Promise((resolve, reject) => {
+      if (_.isString(mdn) && !_.isEmpty(mdn)) {
+        tpslong.setPromise(idenTps , 'mdn', new Long(mdn)).then(resolve, reject);
+      } else {
+        resolve();
+      }
+    })
+  ]);
   return idenTps;
 }
-(async () => {    
-  const idenTps = await buildIdendTps();
-  const str = await idenTps.toStringPromise();
-  console.dir(str);
+(async () => {
+  /**
+   * Assume
+   * 1. apikey: 24611
+   */
+  const idenTps = await buildIdendTps({
+    ip: '192.168.84.233',
+    lang: 'zh-cn',
+    mdn: '9999990012', // As for apikey=24611, the field should be null.
+    credential: 'tAO+lTVRKdtcVmX9hj1NTqfvz/FCx/vMtx4BfElp',
+    'client-guid': 'dc5b30a4ff374279802a06b26cef9b77'
+  }); //TODO: from http request
+  idenTps.toStringPromise().then(console.dir);
 })();

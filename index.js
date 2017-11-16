6553746767e980165cdf43ff5594073eb0e270e6
stuartZhang <stuartpage@qq.com>
2017-11-17 07:18:34 +0800

    Complete a new function "calcServletName"

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const java = require('java');
-const path = require('path');
-const fs = require('fs');
 const {promisify} = require('util');
 const _ = require('underscore');
-const fsStat = promisify(fs.stat);
+const fs = require('fs');
+const java = require('java');
+const path = require('path');
+const stripJsonComments = require('strip-json-comments');
+//
+const sampleReq = require('./tests/geocode-req-1.json');
+//
 java.asyncOptions = {
   'asyncSuffix': 'Async',     // Don't generate node-style methods taking callbacks
   'syncSuffix': 'Sync',              // Sync methods use the base name(!!)
@@ -32,7 +35,19 @@ const StaticTemplateLibrary = java.import('com.nim.tps.StaticTemplateLibrary');
 const MutableTPSElement = java.import('com.nim.tps.MutableTPSElement');
 const tpsstr = java.import('com.nim.tps.attrio.tpsstr');
 const tpslong = java.import('com.nim.tps.attrio.tpslong');
-
+//
+const fsStat = promisify(fs.stat);
+const fsReadFile = promisify(fs.readFile);
+//
+async function calcServletName({query}){
+  const keys = Object.keys(query);
+  console.assert(keys.length === 1, `Too many querys ${keys.join()} in the same request.`);
+  const [queryName] = keys;
+  const jsonStr = await fsReadFile(path.resolve('./conf/query2servletName.json'));
+  const mapping = JSON.parse(stripJsonComments(jsonStr.toString()));
+  console.assert(mapping[queryName], `miss the servlet name for query name ${queryName}`);
+  return mapping[queryName];
+}
 async function buildIdendTps({ip, lang, mdn, credential, 'client-guid': clientGuid, 'user-agent': userAgent}){
   const idenTps = new MutableTPSElement('iden');
   await Promise.all([
@@ -76,37 +91,27 @@ async function loadTpslib({tpslib}){
     servlet: {
       /*
         servlet name:
-          asr=asr-reco
-          event=proxpoi,global
-          geocode=geocode,global
-          map=map,global
-          movie=proxpoi,global
-          navigation=nav,global
-          proxpoi=proxpoi,global
-          com.nim.nbwa.servlet.vectortile=vector-tile,global
-          proxpoi.scheme=atlasbook-standard-2
-          pronun=pronun
-          singlesearch.scheme=tcs-nbwa-single-search
-          singlesearch.geocode.scheme=tcs-nbwa-geocode-search
-          traffic=proxpoi,sb
-          traffic.notify=traffic-notify,global
-          traffic.scheme=traffic-incident
-          reverseGeocode=reverse-geocode,global
-          sendPlace=send-place-message
-          sendMessage=send-message
-          weather=proxpoi,global
-          metadata=metadata
-          siinglesearch=search
+          servlet.command.geocode-query=geocode,usa
+          servlet.command.reverse-geocode-query=reverse-geocode,usa
+          servlet.command.nav-query=nav,usa
+          servlet.command.proxpoi-query=proxpoi,usa
+          servlet.command.send-place-message-query=send-place-message
+          servlet.command.send-message-query=send-message
+          servlet.command.search-query=search
       */
-      name: 'geocode,global',
+      name: undefined,
       tpslib: '5a5139403cce4a4719428b304739567dab9e11de',
       host: '192.168.84.234'
-    }
+    },
+    body: sampleReq
   }; //TODO: from http request
-  const [idenTps, tpslib] = await Promise.all([
+  const [servletName, idenTps, tpslib] = await Promise.all([
+    calcServletName(request.body),
     buildIdendTps(request.iden),
     loadTpslib(request.servlet)
   ]);
+  request.servlet.name = servletName;
   idenTps.toStringPromise().then(console.log);
   console.dir(tpslib);
+  console.log('Servlet Name:', request.servlet.name);
 })();

ed2a6a3416e72e39e50a8f76f55bca21dd23a1d5
stuartZhang <stuartpage@qq.com>
2017-11-21 23:47:27 +0800

    Add a new test case

--- tests/test-2.js
+++ tests/test-2.js
@@ -4,7 +4,7 @@ const sameleIden = require('./iden-1.json');
 const sampleReq = require('./geocode-req-2.json');
 
 (async () => {
-  console.log('request', sameleIden, sampleReq);
+  console.log('Iden', JSON.stringify(sameleIden, null, 2), '\nRequest', JSON.stringify(sampleReq, null, 2));
   const res = await tpsConnect(sameleIden, sampleReq);
-  console.log('response', res);
+  console.log('Response', JSON.stringify(res, null, 2));
 })();

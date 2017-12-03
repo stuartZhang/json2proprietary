5e1b479d633d1f916ab76502c2d8652b0537649c
stuartZhang <stuartpage@qq.com>
2017-12-03 20:11:51 +0800

    support more search-request format

--- tests/test-3.js
+++ tests/test-3.js
@@ -1,8 +1,19 @@
 const tpsConnect = require('../lib');
 const sameleIden = require('./iden-1.json');
-const sampleReq = require('./ssb-req-1.json');
+let sampleReq, res;
 (async () => {
+  sampleReq = require('./ssb-req-1.json');
   console.log('Iden', JSON.stringify(sameleIden, null, 2), '\nRequest', JSON.stringify(sampleReq, null, 2));
-  const res = await tpsConnect(sameleIden, sampleReq);
+  res = await tpsConnect(sameleIden, sampleReq);
+  console.log('Response', JSON.stringify(res, null, 2));
+  //
+  sampleReq = require('./ssb-req-2.json');
+  console.log('Iden', JSON.stringify(sameleIden, null, 2), '\nRequest', JSON.stringify(sampleReq, null, 2));
+  res = await tpsConnect(sameleIden, sampleReq);
+  console.log('Response', JSON.stringify(res, null, 2));
+  //
+  sampleReq = require('./ssb-req-3.json');
+  console.log('Iden', JSON.stringify(sameleIden, null, 2), '\nRequest', JSON.stringify(sampleReq, null, 2));
+  res = await tpsConnect(sameleIden, sampleReq);
   console.log('Response', JSON.stringify(res, null, 2));
 })();

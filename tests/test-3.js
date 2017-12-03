fb9bf333320cbbf0b6757bb4f696587999cc2b5d
stuartZhang <stuartpage@qq.com>
2017-12-03 19:42:16 +0800

    Add ssb1 test case

---
+++ tests/test-3.js
@@ -0,0 +1,8 @@
+const tpsConnect = require('../lib');
+const sameleIden = require('./iden-1.json');
+const sampleReq = require('./ssb-req-1.json');
+(async () => {
+  console.log('Iden', JSON.stringify(sameleIden, null, 2), '\nRequest', JSON.stringify(sampleReq, null, 2));
+  const res = await tpsConnect(sameleIden, sampleReq);
+  console.log('Response', JSON.stringify(res, null, 2));
+})();
